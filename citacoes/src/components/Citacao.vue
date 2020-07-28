9a4dbd9f96dccd3017bf672d216ecb5492a78477
higorasilverio <higorasilverio@outlook.com>
2020-07-28 18:37:14 -0300

    Final files about advanced components features

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('created')
+    },
+    destroyed() {
+        console.log('destroyed')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
+    activated() {
+        console.log('activated')
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
